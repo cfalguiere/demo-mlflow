7cb491681984aebf47930c16623a647d39fad42d
Claude Falguiere <cfalguiere@gmail.com>
2019-10-24 00:28:18 +0200

    fixed typo

--- setup/mysql_secure_installation_template.sql
+++ setup/mysql_secure_installation_template.sql
@@ -1,6 +1,8 @@
+cat <<ABCDEF
 UPDATE mysql.user SET Password=PASSWORD('${MYSQL_ADMIN_PASSWORD}') WHERE User='root';
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 FLUSH PRIVILEGES;
+ABCDEF
