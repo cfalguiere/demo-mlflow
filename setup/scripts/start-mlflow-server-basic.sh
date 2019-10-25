533d642053c14bcda15cb16956265d16f029b67f
Claude Falguiere <cfalguiere@gmail.com>
2019-10-25 21:26:01 +0200

    fixed log path

--- setup/scripts/start-mlflow-server-basic.sh
+++ setup/scripts/start-mlflow-server-basic.sh
@@ -1,1 +1,1 @@
-mlflow server --backend-store-uri /opt/mlflow/mlruns --default-artifact-root /opt/mlflow/mlruns --host 0.0.0.0 2>&1 | tee -a /var/log/mlflow/mlflowserver.out
+mlflow server --backend-store-uri /opt/mlflow/mlruns --default-artifact-root /opt/mlflow/mlruns --host 0.0.0.0 2>&1 | tee -a /var/log/demo-mlflow/mlflowserver.out
