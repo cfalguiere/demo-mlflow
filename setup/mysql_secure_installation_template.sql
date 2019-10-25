bf2cd5ba90e10378388d824607e0f74e93e09640
Claude Falguiere <cfalguiere@gmail.com>
2019-10-25 19:52:00 +0200

    change mysql root auth to socket

--- setup/mysql_secure_installation_template.sql
+++ setup/mysql_secure_installation_template.sql
@@ -1,5 +1,5 @@
 cat <<ABCDEF
-UPDATE mysql.user SET Password=PASSWORD('${MYSQL_ADMIN_PASSWORD}') WHERE User='root';
+-- UPDATE mysql.user SET Password=PASSWORD('${MYSQL_ADMIN_PASSWORD}') WHERE User='root';
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE IF EXISTS test;
