f4249deb62fda738b9a781596b084f753a88ea4a
Claude Falguiere <cfalguiere@gmail.com>
2019-10-27 18:58:09 +0100

    added comments

--- setup/scripts/start-mlflow-server-db.sh
+++ setup/scripts/start-mlflow-server-db.sh
@@ -1,2 +1,4 @@
 # TODO check var
-mlflow server --backend-store-uri mysql://mlflow:${MLFLOW_DB_PASSWORD}@127.0.0.1/mlflow --default-artifact-root /opt/mlflow/mlruns --host 0.0.0.0 2>&1 | tee -a /var/log/demo-mlflow/mlflowserver.out  
+# mysql db mlflow and user mlflow must have been created
+# var MLFLOW_DB_PASSWORD must have been set to the password of te mlflow account
+mlflow server --backend-store-uri mysql://mlflow:${MLFLOW_DB_PASSWORD}@127.0.0.1/mlflow --default-artifact-root /opt/mlflow/mlruns --host 0.0.0.0 2>&1 | tee -a /var/log/demo-mlflow/mlflowserver.out
